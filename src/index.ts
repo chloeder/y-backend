352cb9f7f173438a0254f6f06a69184fd3ca700e
Steward Lumowa <stewardyohanes29@gmail.com>
2024-06-19 09:32:49 +0800

    chore(backend) fixing for deployment

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ import userRouter from "./routes/v1/user.route";
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "*", credentials: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
